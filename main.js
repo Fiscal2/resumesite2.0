e44a4e0e9a5e96eb05767e631bb7d2c23f1982a7
Fiscal2 <zacharyross3@gmail.com>
2022-05-17 21:24:17 -0400

    finished function

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const response = fetch("https://rickandmortyapi.com/api/character/1");
 
 async function fetchCharacter() {
     
-     
+     const response = await fetch("https://rickandmortyapi.com/api/character/1")
+     const data = await response.json();
+     console.log(data);
 
 }
