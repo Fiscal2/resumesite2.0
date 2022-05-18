626c6aa15ccc345b8589e054dce0e17e32745b17
Fiscal2 <zacharyross3@gmail.com>
2022-05-17 21:49:18 -0400

    added const rickDiv

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ async function fetchCharacter() {
      const response = await fetch("https://rickandmortyapi.com/api/character/1")
      const data = await response.json();
      console.log(data);
-
+     const rickDiv = document.querySelector('#rick');
 }
 
 fetchCharacter();
